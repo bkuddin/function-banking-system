07620c6ddf65ed5882f66f344c9d963fb201d92e
bkuddin <mdbkuddin@gmail.com>
2023-08-15 22:43:56 +0600

    Customized withdraw total value in Js

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,5 +1,19 @@
 document.getElementById('withdraw-button').addEventListener('click', function(){
     const withdrawInput = document.getElementById('withdraw-input-field');
     let withdrawNewInputValue = withdrawInput.value;
+    withdrawNewInputValue = parseFloat(withdrawNewInputValue)
     // console.log(withdrawNewInputValue);
+
+    withdrawInput.value = '';
+
+    const withdrawDisplay = document.getElementById('withdraw-display');
+    let withdrawPreviousDisplayValue = withdrawDisplay.innerText;
+    withdrawPreviousDisplayValue = parseFloat(withdrawPreviousDisplayValue)
+
+    // Calculate 
+
+    const currentWithdrawTotal = withdrawPreviousDisplayValue + withdrawNewInputValue;
+    withdrawDisplay.innerText = currentWithdrawTotal;
+
+
 })
