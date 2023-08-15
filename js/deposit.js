6fcf9b8d9a7e51ae10762a95cf97472716aa4372
bkuddin <mdbkuddin@gmail.com>
2023-08-15 22:34:13 +0600

    Customized deposit total balance in js

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,5 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
-
+   //1. Deposit Input Field   
    const takeDepositInput = document.getElementById('deposite-input-field');
    let takeDepositNewInput = takeDepositInput.value;
    takeDepositNewInput = parseFloat(takeDepositNewInput)
@@ -7,13 +7,29 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
    takeDepositInput.value= '';
 
-
+    //2. Deposit Previous Number
     const depositPreviousTotal = document.getElementById('deposit-total');
     let depositPreviousTotalValue = depositPreviousTotal.innerText;
     depositPreviousTotalValue = parseFloat(depositPreviousTotalValue);
     // console.log(depositPreviousTotalValue)
-
+    
+    // 3. Calculation
     const currentDeposit = takeDepositNewInput + depositPreviousTotalValue;
-
+    // 4. Setup
     depositPreviousTotal.innerText = currentDeposit;
+
+    // 5. Deposit Total Balance
+
+    const depositTotalBalance = document.getElementById('deposit-total-balance');
+    let depositTotalBalancePreviousValue = depositTotalBalance.innerText;
+    depositTotalBalancePreviousValue = parseFloat(depositTotalBalancePreviousValue);
+
+    // Calculate Current Total Deposit Balance
+
+    const currentDepositTotalBalance = depositTotalBalancePreviousValue + takeDepositNewInput;
+    depositTotalBalance.innerText = currentDepositTotalBalance 
+    
+
+
+
 })
