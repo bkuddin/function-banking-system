e5ab9fa4a583101f35208ce577ed82dd67a40339
bkuddin <mdbkuddin@gmail.com>
2023-08-15 22:37:50 +0600

    Take withdraw input field in JS

---
+++ js/withdraw.js
@@ -0,0 +1,5 @@
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input-field');
+    let withdrawNewInputValue = withdrawInput.value;
+    // console.log(withdrawNewInputValue);
+})
