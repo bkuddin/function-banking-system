42287fa4d16642c4617f134869c25b63c60d784e
bkuddin <mdbkuddin@gmail.com>
2023-08-16 15:55:47 +0600

    Updated

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,8 +1,8 @@
 
 
 
-function getInputValueByID(elementId){
-    const newInput = document.getElementById(elementId);
+function getInputValueByID(inputId){
+    const newInput = document.getElementById(inputId);
     let newInputValue = newInput.value;
     newInputValue = parseFloat(newInputValue);
     newInput.value = '';
@@ -11,11 +11,23 @@ function getInputValueByID(elementId){
 
 }
 
+function getTextElementValue(elementId){
+    const withdrawTextElement = document.getElementById(elementId);
+    let withdrawPreviousTextValue = withdrawTextElement.innerText;
+    withdrawPreviousTextValue = parseFloat(withdrawPreviousTextValue)
+
+    return withdrawPreviousTextValue;
+}
+
 // All Functions Call in Event
 
 document.getElementById('withdraw-button').addEventListener('click', function(){
 // Function Call:  Get Withdraw input Field Value 
   const withdrawInput = getInputValueByID('withdraw-input-field');
+
+// Function Call: Get Withdraw Text Field Value
+
+  const withdrawText = getTextElementValue('withdraw-display');
 
 
 
