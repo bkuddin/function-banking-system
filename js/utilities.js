1862f1294aedfa5bc6a9169b60e66674ccb2e366
bkuddin <mdbkuddin@gmail.com>
2023-08-16 15:31:09 +0600

    All functions are now located in Utilities.js for better experience

---
+++ js/utilities.js
@@ -0,0 +1,26 @@
+function getInputValueByID(inputId){
+    const inputValue = document.getElementById(inputId);
+    let newInputValue = inputValue.value;
+    newInputValue = parseFloat(newInputValue);
+   
+    inputValue.value = '';
+ 
+    return newInputValue;
+ }
+ 
+ 
+ function getPreviousTextElementById(elementId){
+    const previousTextElement = document.getElementById(elementId);
+    let previousTextElementValue = previousTextElement.innerText;
+    previousTextElementValue = parseFloat(previousTextElementValue);
+ 
+    return previousTextElementValue;
+ }
+ 
+ // Set: Two things are needed here. 1. We have to get to the element I want to set. 2. The new value I want to set.
+ 
+ function setTextElementValueByID(elementId, newValue){
+    const previousTextElement = document.getElementById(elementId);
+    previousTextElement.innerText = newValue;
+ 
+ }
